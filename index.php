52840d67e6e137ad571445fe745a3e4e090f5b8f
Jeon Hyun-chul <julyfool@gmail.com>
2012-01-09 14:37:05 +0900

    make default index file.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width" />
+	<title>Dot.</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
