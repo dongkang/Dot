4698cf1999b0100545fc0acc7396e03e9eb7f50e
Hunjong Yoo <romanyblue@gmail.com>
2012-01-12 00:56:47 +0900

    add event handler to toolbar elements

--- static/script.js
+++ static/script.js
@@ -8,4 +8,29 @@ $(document).ready(function() {
 	$(window).resize(function(){
 		controlLayout();
 	});
+
+	// handle colorPicker
+	$("#colorPicker").click(function() {
+		console.log("open colorPicker!")
+	});
+
+	// change mode
+	$("input[name=mode]").change(function() {
+		console.log("change mode!");
+	});
+
+	// undo action
+	$("#btnUndo").click(function() {
+		console.log("undo!");
+	});
+
+	// redo action
+	$("#btnRedo").click(function() {
+		console.log("redo!");
+	});
+
+	// show menu
+	$("#btnMenu").click(function() {
+		console.log("show menu!");
+	});
 });
