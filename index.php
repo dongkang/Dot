1f2a4b93f1256caa662d903949bd2122772ee727
Hunjong Yoo <romanyblue@gmail.com>
2012-01-11 23:18:55 +0900

    import style.css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width" />
 	<title>Dot.</title>
+	<link href="./static/style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
