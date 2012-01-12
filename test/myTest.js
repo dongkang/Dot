a0173d9b123401aee73f531d5ec3a34c27ad1e64
Jeon Hyun-chul <julyfool@gmail.com>
2012-01-12 22:24:08 +0900

    first works for canvas module

--- test/myTest.js
+++ test/myTest.js
@@ -1,5 +1,19 @@
-describe("runner 테스트", function() {
-	it("무조건 true인지 확인한다.", function() {
+describe("Test Runner", function() {
+	it("무조건 true인지 확인한다", function() {
 		expect(true).toBeTruthy();
 	});
+});
+
+describe("Canvas Model", function() {
+	var canvas = new Canvas;
+
+	it("기본값으로 초기화 된다", function() {
+		expect(canvas.get("width")).toBe(32);
+		expect(canvas.get("height")).toBe(32);
+	});
+
+	it("브러쉬 색상이 변경 된다.", function() {
+		
+	});
+
 });
--- test/runner.php
+++ test/runner.php
@@ -3,9 +3,17 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Dot. Test</title>
+	<!-- Test Framework -->
 	<link type="text/css" rel="stylesheet" href="../lib/jasmine/jasmine.css" />
 	<script type="text/javascript" src="../lib/jasmine/jasmine.js"></script>
 	<script type="text/javascript" src="../lib/jasmine/jasmine-html.js"></script>
+	<!-- Application Library -->
+	<script type="text/javascript" src="../lib/jquery.js"></script>
+	<script type="text/javascript" src="../lib/underscore.js"></script>
+	<script type="text/javascript" src="../lib/backbone.js"></script>
+	<!-- Application Code -->
+	<script type="text/javascript" src="../static/canvas.js"></script>
+	<!-- Test Spec -->
 	<script type="text/javascript" src="myTest.js"></script>
 </head>
 
